efe520ce1636c69378df89a16e80f1606908475a
Razz <pawlowski.a.m@gmail.com>
2023-02-09 13:48:52 -0500

    aaa

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -2,8 +2,7 @@ package utils;
 
 public class Constants {
 
-    //public static final String CONF_FILEPATH = System.getProperty("user.dir") + "/src/test/resources/config/config.properties";
-    public static final String CONF_FILEPATH = System.getProperty("https://github.com/RazielEL/TestNGUbuntu/blob/9ca63d071d4ad2e09e5a9085fd751ac0ad67de2b/src/test/resources/config/config.properties");
+    public static final String CONF_FILEPATH = "src/test/resources/config/config.properties";
     public static final int IMPLICIT_WAIT = 10;
 
 
